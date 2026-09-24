a57bc0f8b22d5cb6a7ec9f749a32ceee19d76690
Guillaume Blin <lemail2guillaume@gmail.com>
2026-09-24 14:40:10 +0200

    Initialize Express server with basic routes

---
+++ td4/app-express.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const host = 'localhost';
+const port = 8080;
+
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello !');
+});
+
+app.get('/home', function(req, res) {
+  res.send('Maison :-)');
+});
+
+app.get('/user/:uid', function(req, res) {
+  res.send('Bonjour ' + req.params.uid);
+});
+
+app.use(function(req, res){
+  res.status(404).send('Page introuvable !');
+});
+
+app.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`)
+});
